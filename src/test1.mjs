efc7178962fee4c2d51f66264b7bdce57eb980af
Mujtaba <mujtaba.freelance.work.acc@gmail.com>
2024-02-06 23:39:26 +0500

    added few code for practice

---
+++ src/test1.mjs
@@ -0,0 +1,9 @@
+let a="mujtaba";
+let b="farooq";
+let c="akhtar";
+let d="noman";
+export {a};
+export {b};
+export {c};
+export {d};
+
---
+++ src/test2.mjs
@@ -0,0 +1,14 @@
+import * as a  from '../src/test1.mjs'
+
+import React from 'react'
+
+const test2 = (a) => {
+    console.log(a)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default test2
